b605170562ae053bb3fc78c3656d2b967956cf8d
Christian Nagel <christian@christiannagel.com>
2016-05-29 09:38:55 +0200

    dependency injection sample

---
+++ Foundation/DependencyInjection/src/DependencyInjection/HelloController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DependencyInjection
+{
+    public class HelloController
+    {
+        private readonly IGreetingService _greetingService;
+        public HelloController(IGreetingService greetingService)
+        {
+            if (greetingService == null) throw new ArgumentNullException(nameof(greetingService));
+            _greetingService = greetingService;
+        }
+
+        public string Action(string name) =>
+            _greetingService.Greeting(name);
+    }
+}
