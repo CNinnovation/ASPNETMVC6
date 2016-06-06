f51e8e1f233dacfe71b68ffd3a6cfca0c3e5e8de
Christian Nagel <christian@christiannagel.com>
2016-06-06 11:58:28 +0200

    add configuration sample

---
+++ Foundation/ConfigurationSample/src/ConfigurationSample/Program.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConfigurationSample
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            SetupConfiguration(args);
+            ReadConfiguration();
+        }
+
+        private static void ReadConfiguration()
+        {
+            string value1 = Config["mysetting1"];
+            Console.WriteLine($"read config for mysetting1: {value1}");
+            string value2 = Config["a"];
+            Console.WriteLine($"read config for a: {value2}");
+            string connectionString = Config["ConnectionStrings:DefaultConnection"];
+            Console.WriteLine($"read config for connection string: {connectionString}");
+            IConfigurationSection section = Config.GetSection("ConnectionStrings");
+
+            string asecret = Config["mysecret"];
+            Console.WriteLine($"read this secret from user secrets: {asecret}");
+        }
+
+        private static void SetupConfiguration(string[] args)
+        {
+            IConfigurationBuilder builder = new ConfigurationBuilder()
+                .SetBasePath(@"C:\Workshop\sources\Foundation\ConfigurationSample\src\ConfigurationSample\")
+                .AddJsonFile("mymandatorysettings.json")
+                .AddJsonFile("myoptionalsettings.json", optional: true)
+                .AddCommandLine(args)
+                .AddEnvironmentVariables();
+
+            builder.AddUserSecrets();
+
+#if DEVELOP
+
+#endif
+            Config = builder.Build();
+        }
+
+        public static IConfigurationRoot Config { get; private set; }
+    }
+}
