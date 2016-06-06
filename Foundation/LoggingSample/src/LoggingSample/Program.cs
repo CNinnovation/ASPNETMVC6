abc59f6aee2fef60cf45c286e0e99a17dee185aa
Christian Nagel <christian@christiannagel.com>
2016-06-06 12:47:49 +0200

    add logging sample

---
+++ Foundation/LoggingSample/src/LoggingSample/Program.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LoggingSample
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            InitLogging();
+
+            Foo();
+
+        }
+
+        private static void Foo()
+        {
+            Logger.LogInformation("Foo started");
+            Bar("one");
+
+            Logger.LogInformation("Foo finished");
+        }
+
+        private static void Bar(string v)
+        {
+            Logger.LogError("some error in Bar");
+        }
+
+        private static void InitLogging()
+        {
+            var loggerFactory = new LoggerFactory().AddConsole().AddDebug();
+
+            Logger = loggerFactory.CreateLogger<Program>();
+        }
+
+        public static ILogger Logger { get; private set; }
+    }
+}
---
+++ Foundation/LoggingSample/src/LoggingSample/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LoggingSample")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e583be2f-e225-43a5-85a2-a9306491d57c")]
